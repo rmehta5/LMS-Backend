47997302596f29510084dd949bdfdf08a7457302
Ritik Mehta <mehta2727k@gmail.com>
2021-09-22 20:43:28 +1000

    author and book model created

--- models/book.js
+++ models/book.js
@@ -4,9 +4,10 @@ const { Author } = require("./author");
 
 const bookSchema = new mongoose.Schema({
 	name: { type: String, required: true },
-	author: { type: mongoose.Schema.Types.ObjectId, ref: Author },
+	author: { type: [mongoose.Schema.Types.ObjectId], ref: Author },
 	stockCount: { type: Number, required: true },
 	category: { type: String, required: true },
+	rentalRate: { type: Number, required: true },
 });
 
 const Book = mongoose.model("Book", bookSchema);
@@ -16,6 +17,7 @@ function validateBook(book) {
 		name: Joi.string().required(),
 		author: Joi.string().required(),
 		stockCount: Joi.number().required(),
+		rentalRate: Joi.number().required(),
 		category: Joi.string().required(),
 	};
 
